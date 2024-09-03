05bace0ee97151e4e899833b50c86920bea5f9ae
qw <qw>
2024-09-03 20:29:53 +0300

    add

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,7 @@ def home_view(request):
     """Контроллер для отображения домашней страницы."""
     return render(request, 'catalog/home.html')
 
+
 def contact_view(request):
     """Контроллер для отображения страницы с контактной информацией."""
     return render(request, 'catalog/contact.html')
